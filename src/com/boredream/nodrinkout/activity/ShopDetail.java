0eabe390524dfb47265f703a6e7b91e5442f84b4
boredream <48262906@qq.com>
2015-03-26 16:34:50 +0800

    ic更改

---
+++ src/com/boredream/nodrinkout/activity/ShopDetail.java
@@ -0,0 +1,16 @@
+package com.boredream.nodrinkout.activity;
+
+import android.os.Bundle;
+
+import com.boredream.nodrinkout.BaseActivity;
+
+public class ShopDetail extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+}
