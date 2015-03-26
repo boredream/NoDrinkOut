def485770bb3d072231f9be492ebedb7c253d238
mollysama <532825294@qq.com>
2015-03-26 16:26:07 +0800

    发现
    发现

---
+++ src/com/boredream/nodrinkout/activity/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.boredream.nodrinkout.activity;
+
+import android.os.Bundle;
+
+import com.boredream.nodrinkout.BaseActivity;
+import com.boredream.nodrinkout.R;
+
+public class SearchActivity extends BaseActivity{
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_search);
+}
+}
