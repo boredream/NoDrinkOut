e0859c9cd039ee673f640f6be67db9442c620ece
boredream <48262906@qq.com>
2015-04-16 18:01:09 +0800

    plv switch tab bug

--- src/com/boredream/nodrinkout/fragment/ShopFragment.java
+++ src/com/boredream/nodrinkout/fragment/ShopFragment.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
-
 import cn.bmob.v3.datatype.BmobGeoPoint;
 
 import com.boredream.nodrinkout.BaseApplication;
@@ -33,28 +32,28 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 	private View view;
 	private Pull2RefreshListView plv_shop_most_follow;
 	private View footViewMostFollow;
-//	private Pull2RefreshListView plv_shop_nearest;
-//	private View footViewNearest;
+	private Pull2RefreshListView plv_shop_nearest;
+	private View footViewNearest;
 	private RadioGroup rg_shop;
 	
 	/**
 	 * 1-最热 2-最近
 	 */
 	private int curType = 1;
-	private boolean isLoadingNextPage = false;
-	private List<CoffeeShop> showShops = new ArrayList<CoffeeShop>();
 	
 	////////////////////////////////////////////////
 
 	private ShopAdapter mostFollowShopAdapter;
 	private List<CoffeeShop> mostFollowShops = new ArrayList<CoffeeShop>();
 	private int mostFollowCurPage = 1;
+	private boolean isLoadingMostFollow = false;
 	
 	////////////////////////////////////////////////
 	
-//	private ShopAdapter nearestShopAdapter;
+	private ShopAdapter nearestShopAdapter;
 	private List<CoffeeShop> nearestShops = new ArrayList<CoffeeShop>();
 	private int nearestCurPage = 1;
+	private boolean isLoadingNearest = false;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,7 +62,8 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 
 		initView();
 		setData();
-		loadData(1);
+		
+		showMostFollow();
 		
 		return view;
 	}
@@ -74,25 +74,20 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 		rg_shop.setOnCheckedChangeListener(this);
 		
 		plv_shop_most_follow = (Pull2RefreshListView) view.findViewById(R.id.plv_shop_most_follow);
-//		plv_shop_nearest = (Pull2RefreshListView) view.findViewById(R.id.plv_shop_nearest);
+		plv_shop_nearest = (Pull2RefreshListView) view.findViewById(R.id.plv_shop_nearest);
 		
 		footViewMostFollow = View.inflate(activity, R.layout.footview_loading, null);
-//		footViewNearest = View.inflate(activity, R.layout.footview_loading, null);
+		footViewNearest = View.inflate(activity, R.layout.footview_loading, null);
 	}
 	
 	private void setData() {
-		mostFollowShopAdapter = new ShopAdapter(activity, showShops);
+		mostFollowShopAdapter = new ShopAdapter(activity, mostFollowShops);
 		plv_shop_most_follow.setAdapter(mostFollowShopAdapter);
 		plv_shop_most_follow.setOnRefreshListener(new OnRefreshListener<ListView>() {
 
 			@Override
 			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-				if(curType == 1) {
-					mostFollowShops.clear();
-				} else {
-					nearestShops.clear();
-				}
-				loadData(1);
+				loadData(curType, 1);
 			}
 		});
 		plv_shop_most_follow.setOnItemClickListener(new SimpleOnItemClickListener(activity));
@@ -101,41 +96,40 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 
 					@Override
 					public void onLastItemVisible() {
-						if (!isLoadingNextPage) {
-							loadData(curType == 1?(mostFollowCurPage + 1):(nearestCurPage + 1));
+						if (!isLoadingMostFollow) {
+							loadData(curType, mostFollowCurPage + 1);
 						}
 					}
 				});
 
-//		nearestShopAdapter = new ShopAdapter(activity, nearestShops);
-//		plv_shop_nearest.setAdapter(nearestShopAdapter);
-//		plv_shop_nearest.setOnRefreshListener(new OnRefreshListener<ListView>() {
-//
-//			@Override
-//			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
-//				nearestShops.clear();
-//				loadData(1);
-//			}
-//		});
-//		plv_shop_nearest.setOnItemClickListener(new SimpleOnItemClickListener(activity));
-//		plv_shop_nearest.setOnLastItemVisibleListener(
-//				new OnLastItemVisibleListener() {
-//
-//					@Override
-//					public void onLastItemVisible() {
-//						isLoadingNextPage = true;
-//
-//						if (!isLoadingNextPage) {
-//							loadData(nearestCurPage + 1);
-//						}
-//					}
-//				});
+		nearestShopAdapter = new ShopAdapter(activity, nearestShops, 2);
+		plv_shop_nearest.setAdapter(nearestShopAdapter);
+		plv_shop_nearest.setOnRefreshListener(new OnRefreshListener<ListView>() {
+
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				loadData(curType, 1);
+			}
+		});
+		plv_shop_nearest.setOnItemClickListener(new SimpleOnItemClickListener(activity));
+		plv_shop_nearest.setOnLastItemVisibleListener(
+				new OnLastItemVisibleListener() {
+
+					@Override
+					public void onLastItemVisible() {
+						isLoadingNearest = true;
+
+						if (!isLoadingNearest) {
+							loadData(curType, nearestCurPage + 1);
+						}
+					}
+				});
 
 	}
 
-	private void loadData(final int pageIndex) {
-		isLoadingNextPage = true;
-		if (curType == 1) {
+	private void loadData(final int type, final int pageIndex) {
+		if (type == 1) {
+			isLoadingMostFollow = true;
 			BmobApi.queryShopsWhere(activity, "followCount", pageIndex,
 					new FindSimpleListener<CoffeeShop>(activity, loadDialog) {
 
@@ -143,18 +137,34 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 						public void onSuccess(List<CoffeeShop> arg0) {
 							super.onSuccess(arg0);
 
-							showListData(arg0, pageIndex);
+							// 如果成功加载数据了,更新当前页索引,否则不变
+							mostFollowCurPage = pageIndex;
+							if(pageIndex == 1) {
+								mostFollowShops.clear();
+							}
+
+							mostFollowShops.addAll(arg0);
+							mostFollowShopAdapter.notifyDataSetChanged();
+							plv_shop_most_follow.onRefreshComplete();
+							
+							if(arg0.size() == CommonConstants.COUNT_PER_PAGE) {
+								addFootView(plv_shop_most_follow, footViewMostFollow);
+							} else {
+								removeFootView(plv_shop_most_follow, footViewMostFollow);
+							}
 						}
 
 						@Override
 						public void onFinish() {
 							super.onFinish();
 
-							isLoadingNextPage = false;
+							isLoadingMostFollow = false;
 						}
 
 					});
-		} else if(curType == 2) {
+		} else if(type == 2) {
+			isLoadingNearest = true;
+			
 			BmobGeoPoint geo = new BmobGeoPoint(BaseApplication.curLocation.longitude, 
 					BaseApplication.curLocation.latitude);
 			
@@ -165,14 +175,28 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 				public void onSuccess(List<CoffeeShop> arg0) {
 					super.onSuccess(arg0);
 					
-					showListData(arg0, pageIndex);
+					// 如果成功加载数据了,更新当前页索引,否则不变
+					nearestCurPage = pageIndex;
+					if(pageIndex == 1) {
+						nearestShops.clear();
+					}
+					
+					nearestShops.addAll(arg0);
+					nearestShopAdapter.notifyDataSetChanged();
+					plv_shop_nearest.onRefreshComplete();
+					
+					if(arg0.size() == CommonConstants.COUNT_PER_PAGE) {
+						addFootView(plv_shop_nearest, footViewNearest);
+					} else {
+						removeFootView(plv_shop_nearest, footViewNearest);
+					}
 				}
 				
 				@Override
 				public void onFinish() {
 					super.onFinish();
 					
-					isLoadingNextPage = false;
+					isLoadingNearest = false;
 				}
 				
 			});
@@ -180,29 +204,6 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 		}
 	}
 	
-	private void showListData(List<CoffeeShop> shops, int pageIndex) {
-		if(curType == 1) {
-			// 如果成功加载数据了,更新当前页索引,否则不变
-			mostFollowCurPage = pageIndex;
-			mostFollowShops.addAll(shops);
-			mostFollowShopAdapter.setDatas(mostFollowShops);
-		} else {
-			// 如果成功加载数据了,更新当前页索引,否则不变
-			nearestCurPage = pageIndex;
-			nearestShops.addAll(shops);
-			mostFollowShopAdapter.setDatas(nearestShops);
-		}
-
-		mostFollowShopAdapter.notifyDataSetChanged();
-		plv_shop_most_follow.onRefreshComplete();
-		
-		if(shops.size() == CommonConstants.COUNT_PER_PAGE) {
-			addFootView(plv_shop_most_follow, footViewMostFollow);
-		} else {
-			removeFootView(plv_shop_most_follow, footViewMostFollow);
-		}
-	}
-
 	private void addFootView(PullToRefreshListView plv, View footView) {
 		ListView lv = plv.getRefreshableView();
 		if(lv.getFooterViewsCount() == 1) {
@@ -221,29 +222,36 @@ public class ShopFragment extends BaseFragment implements OnCheckedChangeListene
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
 		switch (checkedId) {
 		case R.id.rb_most_follow:
-			curType = 1;
-			
-//			plv_shop_most_follow.setVisibility(View.VISIBLE);
-//			plv_shop_nearest.setVisibility(View.GONE);
-			
-			if(mostFollowShops.size() == 0) {
-				loadData(1);
-			}
-			
+			showMostFollow();
 			break;
 		case R.id.rb_nearest:
-			curType = 2;
-			
-//			plv_shop_most_follow.setVisibility(View.GONE);
-//			plv_shop_nearest.setVisibility(View.VISIBLE);
-			
-			if(nearestShops.size() == 0) {
-				loadData(1);
-			}
+			showNearest();
 			break;
 
 		default:
 			break;
+		}
+	}
+
+	private void showMostFollow() {
+		curType = 1;
+		
+		plv_shop_most_follow.setVisibility(View.VISIBLE);
+		plv_shop_nearest.setVisibility(View.GONE);
+		
+		if(mostFollowShops.size() == 0) {
+			loadData(curType, 1);
+		}
+	}
+	
+	private void showNearest() {
+		curType = 2;
+		
+		plv_shop_most_follow.setVisibility(View.GONE);
+		plv_shop_nearest.setVisibility(View.VISIBLE);
+		
+		if(nearestShops.size() == 0) {
+			loadData(curType, 1);
 		}
 	}
 }
